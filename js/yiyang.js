e02457f8c675d0ab622f491cc3dd68a97a8a557d
Tong <m13302061411@163.com>
2018-12-16 22:32:21 +0800

    删除多余bgcolor 优化界面 完成版

--- js/yiyang.js
+++ js/yiyang.js
@@ -2,7 +2,7 @@ function zjck() {
 	var ckq = document.getElementById('ckq');
 	var node = ckq.nextSibling;
 	oDiv = document.createElement('div');
-	oDiv.innerHTML = '<div class="cjr kq"><div class="bt">第一位乘机人：<b>刘某某</b><span class="right smallFont">添加常用姓名 <b class="pointer" onclick="deleteCJR(this)" style="color: red;">X</b></span></div><div class="tj"><ul><li><span class="xz">购票类型：</span><select class="input"><option value="" disabled selected hidden>成人票</option></select><span class="smallFont">$2200/成人（含税费）<span class="blueFont smallFont pointer">退改签</span></span></li><li><span class="xz">姓&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;名：</span><input placeholder="刘某某 " class="input"></input><span class="blueFont smallFont pointer">填写说明</span></li><li><span class="xz">护照类型：</span><select class="input"><option value=" " disabled selected hidden>护照</option></select></li><li><span class="xz">证件号码：</span><input placeholder="120120120120120120" class="input" onblur="checkUserName(this)"></input><span class="smallFont redFont"></span></li></ul></div></div>'
+	oDiv.innerHTML = '<div class="cjr kq"><div class="bt"><span class="kt">第一位乘机人：<b>刘某某</b><span class="right smallFont">添加常用姓名 <b class="pointer" onclick="deleteCJR(this)" style="color: red;">X</b></span></span></div><div class="tj"><ul><li><span class="xz">购票类型：</span><select class="input"><option value="" disabled selected hidden>成人票</option></select><span class="smallFont">$2200/成人（含税费）<span class="blueFont smallFont pointer">退改签</span></span></li><li><span class="xz">姓&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;名：</span><input placeholder="刘某某 " class="input"></input><span class="blueFont smallFont pointer">填写说明</span></li><li><span class="xz">护照类型：</span><select class="input"><option value=" " disabled selected hidden>护照</option></select></li><li><span class="xz">证件号码：</span><input placeholder="120120120120120120" class="input" onblur="checkUserName(this)"></input><span class="smallFont redFont"></span></li></ul></div></div>'
 	ckq.parentNode.insertBefore(oDiv, node)
 	node = oDiv.nextSibling;
 	//	alert(ckq.style.height+=180)
@@ -35,5 +35,5 @@ function eMail(mail) {
 }
 
 function deleteCJR(cjr) {
-	cjr.parentNode.parentNode.parentNode.parentNode.removeChild(cjr.parentNode.parentNode.parentNode);
+	cjr.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild(cjr.parentNode.parentNode.parentNode.parentNode);
 }
